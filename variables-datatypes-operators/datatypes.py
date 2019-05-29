798f068dfcd9c094649f19f8224227ce6866eaa1
Hary Morton-Smith <HMorton-Smith@spartaglobal.com>
2019-05-29 10:42:42 +0100

    finish with numerical types

---
+++ variables-datatypes-operators/datatypes.py
@@ -0,0 +1,37 @@
+# Numerical types
+# Numerical types are numbers, integers, floats or composites.
+# in python we will mostly deal with int and float.
+# Declaring int or float we use numerical characters without quotes
+
+num1 = 20
+print(type(num1))
+
+num2 = 20.0
+print(type(num2))
+
+# Operators: we can add, subtract, divide etc.
+result1 = 10 + 10       # add
+result2 = 10 - 2        # subtract
+result3 = 10 * 30       # multiply
+result4 = 20 % 2        # check if there is a remainder
+
+
+print(result1, result2, result3, result4)
+
+# Logical Operators: equate to something
+num_a = 10
+num_b = 10
+num_c = 13
+
+# Greater than
+print(num_b > num_c)
+# Smaller than
+print(num_b < num_c)
+# Greater than or equal
+print(num_b >= num_a)
+# Smaller than or equal
+print(num_b <= num_c)
+#Equate
+print(num_a == num_b)
+print( num_a == num_c)
+
