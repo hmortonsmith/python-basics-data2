974d540906f72619ac3c090a88cf2bd47aa71900
Hary Morton-Smith <HMorton-Smith@spartaglobal.com>
2019-05-29 10:14:35 +0100

    ended the variables lesson

---
+++ variables-datatypes-operators/my-variable.py
@@ -0,0 +1,21 @@
+# A variable is allocated space to store something.
+# In python we can just declare variables by assigning them using
+# the = sign
+
+# Convention:
+# 1. Meaningful names, not x and y
+# 2. Lowercase and use underscores
+
+# Creation and assignment of a variable
+my_variable = "I'm a variable"
+
+# Re-assignment of a variable
+my_variable = "Y not"
+
+print(my_variable)
+print(type(my_variable))
+
+another_var = 10
+
+print(another_var)
+print(type(another_var))
