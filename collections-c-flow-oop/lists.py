b9a4485e9ad7314d3a67a08a7ef52efeb68d1077
Hary Morton-Smith <HMorton-Smith@spartaglobal.com>
2019-05-30 11:27:44 +0100

    finished lists and tuples

--- collections-c-flow-oop/lists.py
+++ collections-c-flow-oop/lists.py
@@ -26,5 +26,27 @@ print(crazy_x_landlord)
 
 # How to remove something from a list - Use .pop()
 # Remove Jane
-crazy_x_landlord.pop([0])
+crazy_x_landlord.pop(0)
 print(crazy_x_landlord)
+
+# Lists can contain anything
+combined_list = [1, '10', 'ten', True, crazy_x_landlord]
+print(combined_list)
+
+# List slicing
+# Used to manage lists
+print(combined_list[0:3])
+print(combined_list[3:])
+print(combined_list[:3])
+# Skip slicing - uses :: and returns from the first index and skips however many you specified
+print(combined_list[0::2])
+
+
+# Tuple is immutable list
+# Defined with ()
+# Same methods but can't append tuple (immutable)
+# Accessed via index
+mortal_enemies = ('Hello Kitty', 'Sailor Moon', 'Iron Man', 'Eye-patch Morty')
+print(type(mortal_enemies))
+# Cannot re-assign an index, it breaks
+# mortal_enemies[1] = 'Goku'
